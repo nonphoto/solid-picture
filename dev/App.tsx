957d0be63731e448baf28adea31cea459afcae81
Jonas Luebbers <jonas@jonasluebbers.com>
2023-04-10 18:37:22 -0700

    Add landscape video

--- dev/App.tsx
+++ dev/App.tsx
@@ -6,31 +6,38 @@ const baseUrl = `https://pub-a7ccd757582f4bd99e5c2364327c0f84.r2.dev`
 
 export default function App() {
   return (
-    <div>
+    <>
       <Picture>
         <Source
           type="video/mp4"
-          placeholderSrc={data.flowerPortrait}
-          srcset={`${baseUrl}/flower-portrait.mp4`}
+          placeholderSrc={data.portrait}
+          srcset={`${baseUrl}/portrait.optimized.mp4`}
           media="(orientation: portrait)"
-          naturalWidth={480}
-          naturalHeight={852}
+          naturalWidth={1080}
+          naturalHeight={1920}
+        />
+        <Source
+          type="video/mp4"
+          placeholderSrc={data.landscape}
+          srcset={`${baseUrl}/landscape.optimized.mp4`}
+          naturalWidth={1280}
+          naturalHeight={720}
         />
         <Img
-          placeholderSrc={data.flowerLandscape}
-          srcset={`${baseUrl}/flower-landscape.jpeg`}
-          naturalWidth={854}
-          naturalHeight={480}
+          placeholderSrc={data.landscape}
+          srcset={`${baseUrl}/landscape.jpg`}
+          naturalWidth={1280}
+          naturalHeight={720}
           width="100%"
         />
       </Picture>
       <Img
-        placeholderSrc={data.flowerLandscape}
-        srcset={`${baseUrl}/flower-landscape.jpeg`}
+        placeholderSrc={data.landscape}
+        srcset={`${baseUrl}/landscape.jpg`}
         naturalWidth={854}
         naturalHeight={480}
         width="100%"
       />
-    </div>
+    </>
   )
 }
