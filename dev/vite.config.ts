bdc82eebade62c00104295a5d0fe6756f6730204
Jonas Luebbers <jonas@jonasluebbers.com>
2023-04-11 14:28:32 -0700

    Enable SSR

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -3,7 +3,7 @@ import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [
-    solidPlugin(),
+    solidPlugin({ ssr: true }),
     {
       name: 'Reaplace env variables',
       transform(code, id) {
